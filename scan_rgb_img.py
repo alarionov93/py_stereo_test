79b700cbf12bc885cd75f914d9f355b23cae86d9
Alexandr Larionov <alarionov93@yandex.ru>
2021-10-29 19:35:07 +0500

    created working algoryghm of sorting points by angle in polar coords

--- scan_rgb_img.py
+++ scan_rgb_img.py
@@ -6,20 +6,33 @@ from matplotlib import pyplot as plt
 from interpol import slice_np_arr
 
 
-def sort_edges(a):
-	shift = 25 # TODO: shift need to be passed to function!!
-	for res, coords in a:
-		quater_center_x = np.mean()
-		X,Y = np.where(res>250)
-		X_ = X+coords[0]*shift
-		Y_ = Y+coords[1]*shift
-		pass
+def get_polar_coords(coords):
+	x = coords[0]
+	y = coords[1]
+	ph = 0
 
-	q = a[0][0]
-	X,Y = np.where(q>250)
-	print(X,Y)
-	return None
+	if x>0 and y>0:
+		ph = math.degrees(math.atan(abs(y/x)))
+	elif x>0 and y<0:
+		ph = 360-math.degrees(math.atan(abs(y/x)))
+	elif x<0 and y<0:
+		ph = 180+math.degrees(math.atan(abs(y/x)))
+	elif x<0 and y>0:
+		ph = 180-math.degrees(math.atan(abs(y/x)))
+	else:
+		print("ФУЦКИНГ ERROR!!")
 
+	ro = int(math.sqrt(x**2 + y**2))
+
+	return (ro, ph)
+
+def sort_edges(edges):
+	X,Y = np.where(edges>250)
+	avg_x, avg_y = (np.mean(X), np.mean(Y))
+	res = [get_polar_coords((x-avg_x, y-avg_y)) for x, y in zip(X,Y)]
+	# print(res)
+
+	return sorted(res, key=lambda e: e[1])
 
 def img_to_fun(img, hbi=84, hbj=94):
 	img_red_c = img[:,:,2]
@@ -27,43 +40,54 @@ def img_to_fun(img, hbi=84, hbj=94):
 	img_blue_c = img[:,:,0]
 	mask_g = (img_green_c < int(hbi)) & (img_blue_c  < int(hbi)) & (img_red_c > int(hbj))
 	edges = cv.Canny(img_red_c * mask_g,200,255)
-
+	# plt.imshow(edges)
 	fun = []
-	shift = 25
-	quaters = slice_np_arr(edges, shift)
+	# quaters = slice_np_arr(edges, shift)
 	curve_cord = []
-	last_x = -1
-	last_y = -1
 
-	quaters = sort_edges(quaters)
+	s_edges = sort_edges(edges)
+	print(s_edges)
+	# # TODO: remove this фуцкинг шыт from отсюда!
+	# X,Y = np.where(edges>250)
+	# avg_x, avg_y = (np.mean(X), np.mean(Y))
+	# res = []
+	# for s in s_edges:
+	# 	x = s[0] + math.cos(s[1])
+	# 	y = s[0] + math.sin(s[1])
+	# 	res += [(x,y)]
 
-	for r, coords in quaters:
-		X,Y = np.where(r>250)
-		X_ = X+coords[0]*shift
-		Y_ = Y+coords[1]*shift
-		a,b,c = np.polyfit(X_,Y_,2)
-		fun += [[a,b,c,X_]]
+	# for r, coords in quaters:
+	# 	X,Y = np.where(r>250)
+	# 	X_ = X+coords[0]*shift
+	# 	Y_ = Y+coords[1]*shift
+	# 	a,b,c = np.polyfit(X_,Y_,2)
+	# 	fun += [[a,b,c,X_]]
+	# print(res)
+	return s_edges
 
-	return fun
+# def find_middle_pts(fun):
+# 	def foo(*args):
+# 		a,b,c,X = args[0]
+# 		return np.array(list(zip(X, X**2*a + X*b + c)))
+# 	res = []
+# 	for i in range(len(fun)-1):
+# 		A,B = fun[i:i+2]
+# 		res += [(foo(A)[-1] + foo(B)[0]) // 2]
 
-def find_middle_pts(fun):
-	def foo(*args):
-		a,b,c,X = args[0]
-		return np.array(list(zip(X, X**2*a + X*b + c)))
-	res = []
-	for i in range(len(fun)-1):
-		A,B = fun[i:i+2]
-		res += [(foo(A)[-1] + foo(B)[0]) // 2]
-
-	return res
+# 	return res
 
 def draw_pts(img,pts):
 	for p in pts:
+		# print(p)
 		cv.circle(img, (int(p[1]), int(p[0])), 4,(255,255,255), -1)
 	cv.imwrite('%s_res.jpg' % (sys.argv[1].split(".")[0]), img)
 
 if __name__ == '__main__':
 	img = cv.imread(sys.argv[1],1)
 	res = img_to_fun(img)
-	draw_pts(img, find_middle_pts(res))
+	# draw_pts(img, res)
+	X = [r[0] for r in res]
+	Y = [r[1] for r in res]
+	plt.plot(Y,X)
+	plt.show()
 
